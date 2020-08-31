11c62aa625ecc525aa6fe4141250e523d1bf5098
willrowe17 <54510998+willrowe17@users.noreply.github.com>
2020-08-31 10:57:17 -0400

    undo remove header

--- src/components/header.js
+++ src/components/header.js
@@ -1,48 +1,48 @@
-// import PropTypes from "prop-types"
-// import React from "react"
+import PropTypes from "prop-types"
+import React from "react"
 
-// import Container from "react-bootstrap/Container"
-// import NavBar from "react-bootstrap/NavBar"
-// import Nav from "react-bootstrap/Nav"
-// import Logo from "../images/gatsby-icon.png"
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-// import { faCoffee } from "@fortawesome/free-solid-svg-icons"
-// import { faGithub } from "@fortawesome/free-brands-svg-icons"
-// import "./header.css"
+import Container from "react-bootstrap/Container"
+import NavBar from "react-bootstrap/NavBar"
+import Nav from "react-bootstrap/Nav"
+import Logo from "../images/gatsby-icon.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCoffee } from "@fortawesome/free-solid-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import "./header.css"
 
-// const Header = ({ siteTitle }) => (
-//   <Container>
-//     <NavBar
-//       fixed="top"
-//       expand="sm"
-//       id="headerCSS"
-//       className="headerCSS navbar-light"
-//     >
-//       <NavBar.Brand id="headerCSS" className="headerCSS" href="/">
-//         {siteTitle}
-//       </NavBar.Brand>
-//       <NavBar.Toggle aria-controls="headerNav" />
-//       <NavBar.Collapse id="headerNav">
-//         <Nav className="ml-auto headerCSS">
-//           {/* change these to gatsby links at some point */}
-//           <Nav.Link id="headerCSS">About</Nav.Link>
-//           <Nav.Link id="headerCSS">Contact</Nav.Link>
-//           <Nav.Link id="headerCSS">Docs</Nav.Link>
-//           <Nav.Link id="headerCSS">
-//             <FontAwesomeIcon icon={faGithub} size="1x" />
-//           </Nav.Link>
-//         </Nav>
-//       </NavBar.Collapse>
-//     </NavBar>
-//   </Container>
-// )
+const Header = ({ siteTitle }) => (
+  <Container>
+    <NavBar
+      fixed="top"
+      expand="sm"
+      id="headerCSS"
+      className="headerCSS navbar-light"
+    >
+      <NavBar.Brand id="headerCSS" className="headerCSS" href="/">
+        {siteTitle}
+      </NavBar.Brand>
+      <NavBar.Toggle aria-controls="headerNav" />
+      <NavBar.Collapse id="headerNav">
+        <Nav className="ml-auto headerCSS">
+          {/* change these to gatsby links at some point */}
+          <Nav.Link id="headerCSS">About</Nav.Link>
+          <Nav.Link id="headerCSS">Contact</Nav.Link>
+          <Nav.Link id="headerCSS">Docs</Nav.Link>
+          <Nav.Link id="headerCSS">
+            <FontAwesomeIcon icon={faGithub} size="1x" />
+          </Nav.Link>
+        </Nav>
+      </NavBar.Collapse>
+    </NavBar>
+  </Container>
+)
 
-// Header.propTypes = {
-//   siteTitle: PropTypes.string,
-// }
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
 
-// Header.defaultProps = {
-//   siteTitle: ``,
-// }
+Header.defaultProps = {
+  siteTitle: ``,
+}
 
-// export default Header
+export default Header
