ff850e43091f35f29c99faf1fd85f78778a68166
willrowe17 <54510998+willrowe17@users.noreply.github.com>
2020-09-10 15:53:35 -0400

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,32 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Jumbotron, Container, Button } from "react-bootstrap"
+import "./customStyle.css"
+
+const JumbotronSection = () => (
+  <Jumbotron fluid className="jumboBackground">
+    <Container>
+      <h1 className="medText">Not Found</h1>
+    </Container>
+  </Jumbotron>
+)
+
+class PrimaryBodySection extends React.Component {
+  render() {
+    return (
+      <Container>
+        <p>Sorry, we couldn't find the page you're looking for.</p>
+      </Container>
+    )
+  }
+}
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <JumbotronSection />
+    <PrimaryBodySection />
   </Layout>
 )
 
